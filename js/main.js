5d20d214ebdb49fbad4abc23e79b68c636a3c40b
fady-elshahat <fady.elshahat@gmail.com>
2021-12-07 18:09:00 +0200

    Marketingadressen Oesterreich Website

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// Change Taps
+if ( window.location.pathname == "/" || window.location.pathname == "/index.html" ) {
+     const tabs = document.querySelectorAll( "#tabs-list .item-tabs" );
+     const tabsArray = Array.from( tabs );
+     const tabsContent = document.querySelectorAll( ".content-tabs > div" );
+     const divsArray = Array.from( tabsContent );
+     const Inputcards = Array.from( document.querySelectorAll( ".card-branch .body-card input" ) );
+
+
+
+     // Move in Tabs
+     tabsArray.forEach( ( el ) => {
+          el.addEventListener( "click", ( e ) => {
+               tabsArray.forEach( ( el ) => {
+                    el.classList.remove( "activate" );
+               } )
+               e.currentTarget.classList.add( "activate" )
+               divsArray.forEach( ( div ) => {
+                    div.style.display = "none"
+               } )
+               document.querySelector( e.currentTarget.dataset.cont ).style.display = "block";
+          } )
+     } )
+
+     // Select Card Branch
+     Inputcards.forEach( ( input ) => {
+
+          input.addEventListener( "click", ( e ) => {
+               const parentElement = input.parentElement;
+               if ( input.checked === true) {
+                    parentElement.parentElement.classList.add( "activate" );
+               } else {
+                    parentElement.parentElement.classList.remove( "activate" );
+               }
+          } )
+     } )
+
+
+}
+
+
+// Eye Password Show
+if ( window.location.pathname === "/pages/sign-up.html" || window.location.pathname === "/pages/sign-in.html" ) {
+     const eyeIconPassword = document.querySelector( ".input-item .pass-icon.btn" );
+     const inputPasswordUp = document.querySelector( ".input-item .input-password" )
+
+     eyeIconPassword.addEventListener( 'click', () => {
+          if ( inputPasswordUp.type === "password" ) {
+               inputPasswordUp.type = "text"
+          } else {
+               inputPasswordUp.type = "password"
+          }
+     } )
+}
