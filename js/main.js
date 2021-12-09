6806ecdecb3fcf1fdd05f1598cad95058a35f8a5
fady-elshahat <fady.elshahat@gmail.com>
2021-12-09 19:03:51 +0200

    build up Check Out page and Modified style.css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Change Taps
+// Change Taps in Main page
 if ( window.location.pathname == "/" || window.location.pathname == "/index.html" ) {
 
      const tabs = document.querySelectorAll( "#tabs-list .item-tabs" );
@@ -57,7 +57,7 @@ if ( window.location.pathname == "/" || window.location.pathname == "/index.html
 
 
 // Eye Password Show
-if ( window.location.pathname === "/pages/sign-up.html" || window.location.pathname === "/pages/sign-in.html" ) {
+if ( window.location.pathname === "/pages/sign-up.html" || window.location.pathname === "/pages/sign-in.html" || window.location.pathname === "/pages/check-out.html" ) {
      const eyeIconPassword = document.querySelector( ".input-item .pass-icon.btn" );
      const inputPasswordUp = document.querySelector( ".input-item .input-password" )
 
@@ -69,3 +69,27 @@ if ( window.location.pathname === "/pages/sign-up.html" || window.location.pathn
           }
      } )
 }
+
+// // Change Taps in Check Out page
+if ( window.location.pathname == "/pages/check-out.html" ) {
+     const tabsCheck = Array.from( document.querySelectorAll( ".tabs-list-check .item-check" ) );
+     const contentCheck = Array.from( document.querySelectorAll( ".content-tabs-check > div" ) );
+
+     tabsCheck.forEach( ( tab ) => {
+          tab.addEventListener( "click", ( e ) => {
+               tabsCheck.forEach( ( el ) => {
+                    el.classList.remove("activate")
+               })
+               const targetElemant = e.currentTarget;
+               console.log(targetElemant);
+               targetElemant.classList.add( "activate" );
+               contentCheck.forEach( ( div ) => {
+                    div.style.display = "none"
+               } )
+               document.querySelector( targetElemant.dataset.check ).style.display = "block";
+
+          } )
+
+     })
+}
+
